221fc3f83b85d11a43659e903e74ecbfd5e1bf51
Akhil Narang <akhilnarang.1999@gmail.com>
2015-06-07 23:22:49 +0530

    Some renaming....
    Change-Id: I074ce32e2105efc07b029c3ad93eb65728b756b7
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- src/com/own/wallpapers/NoWallpaper.java
+++ src/com/own/wallpapers/NoWallpaper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2015 The OwnROM Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.ownrom.wallpapers;
+package com.own.wallpapers;
 
 import android.app.Activity;
 import android.app.WallpaperManager;
